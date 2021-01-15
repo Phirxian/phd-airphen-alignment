75d23e670674992d44b7162affbb94bfb60a4a45
Ovan <phirxian@gmail.com>
2021-01-15 13:54:53 +0100

    remove one debug print

--- airphen/image_processing.py
+++ airphen/image_processing.py
@@ -147,8 +147,6 @@ def affine_transform_linear(S, loaded):
         loaded[i] = cv2.warpAffine(loaded[i], transform[i], dsize)
     pass
     
-    print(np.array(transform))
-    
     return loaded, np.array(transform)
 pass
 
